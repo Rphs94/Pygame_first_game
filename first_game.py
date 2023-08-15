15847d5241ca6f0748f7ac0de2ff70b0d867bcfe
RAph <raphael94260@hotmail.fr>
2023-08-15 15:42:23 +0200

    AAJout d'une raquette contrôlable avec la physique

--- first_game.py
+++ first_game.py
@@ -2,15 +2,38 @@ import sys,pygame
 
 class Ball():
     def __init__(self,pos_x,pos_y):
-        self.pos_x=pos_x
-        self.pos_y=pos_y
+        self.coord=pygame.Vector2(pos_x,pos_y)
+        self.speed_x=1
+        self.speed_y=1
 
+    def get_coord(self):
+        return self.coord
     def get_x(self):
-        return self.pos_x
+        return self.coord.x
 
     def get_y(self):
-        return self.pos_y
-        
+        return self.coord.y
+
+    def get_speed_x(self):
+        return self.speed_x
+    
+    def get_speed_y(self):
+        return self.speed_y
+    
+    def set_x(self,x):
+        self.coord.x=x
+
+    def set_y(self,y):
+        self.coord.y=y
+    
+    def set_speed_x(self,speed_x):
+        self.speed_x=speed_x
+    
+    def set_speed_y(self,speed_y):
+        self.speed_y=speed_y
+    
+
+
 class racket():
 
     def __init__(self,pos_x,pos_y,length,width):
@@ -29,26 +52,29 @@ class racket():
     def get_width(self):
         return self.width
 
-    def set_coord_x(self,x):
-        self.coord.x=x
+    def get_coord_y(self):
+        return self.coord.y
+    
+    def get_coord_x(self):
+        return self.coord.x
 
     def set_coord_y(self,y):
         self.coord.y=y
-    
+
+
+#Paramètres généraux
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+speed_racket=300
 running = True
 dt = 0
 
 #Gestion des balles
 list_ball=[Ball(screen.get_width()/2,screen.get_height()/2)]
-list_pos=[]
-for ball in list_ball:
-    list_pos.append([pygame.Vector2(ball.get_x(),ball.get_y()),1,1])
 
 #Gestion des rackets
-racket_1=racket(screen.get_width()/5,screen.get_height()/2,100,30)
+racket_1=racket(screen.get_width()/10,screen.get_height()/2,100,30)
 
 
 while running:
@@ -59,38 +85,56 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
     
     #Mouvement des rackets
-    pygame.draw.line(screen,"red",racket_1.get_coord_bottom(),racket_1.get_coord_top(),racket_1.get_width())
+    pygame.draw.line(screen,"white",racket_1.get_coord_bottom(),racket_1.get_coord_top(),racket_1.get_width())
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_z]:
+        racket_1.set_coord_y(racket_1.get_coord_y()-speed_racket*dt)
+    elif keys[pygame.K_s]:
+        racket_1.set_coord_y(racket_1.get_coord_y()+speed_racket*dt)
+
+
 
 
     #Mouvement des balles
-    for player_pos in list_pos :
-        pygame.draw.circle(screen, "red", player_pos[0], 30)
-        #Déplacement selon x
-        if player_pos[0].x<screen.get_width()-30 and player_pos[1]==1: 
-            player_pos[0].x+=300*dt
-        elif player_pos[0].x>=screen.get_width()-30 and player_pos[1]==1 :
-            player_pos[1]=-1
-        elif player_pos[0].x>=screen.get_width()-30 and player_pos[1]==-1 :
-            player_pos[0].x-=300*dt
-        elif player_pos[0].x>0+30 and player_pos[1]==-1: 
-            player_pos[0].x-=300*dt
-        elif player_pos[0].x<=0+30 and player_pos[1]==-1: 
-            player_pos[1]=1
+    for ball in list_ball :
+        pygame.draw.circle(screen, "white", ball.get_coord(), 30)
+
+    #Interaction de la balle avec le mur de droite
+        if ball.get_x()<screen.get_width()-30 and ball.get_speed_x()==1: 
+            ball.set_x(ball.get_x()+300*dt)
+        elif ball.get_x()>=screen.get_width()-30 and ball.get_speed_x()==1 :
+            ball.set_speed_x(-1)
+        elif ball.get_x()>=screen.get_width()-30 and ball.get_speed_x()==-1 :
+            ball.set_x(ball.get_x()-300*dt)
+        elif ball.get_x()>0+30 and ball.get_speed_x()==-1: 
+            ball.set_x(ball.get_x()-300*dt)
+        elif ball.get_x()<=0+30 and ball.get_speed_x()==-1: 
+            ball.set_speed_x(1)
+    #Interaction de la balle avec la raquette gauche 
     
-        #Déplacement selon y
-        if player_pos[0].y<screen.get_height()-30 and player_pos[2]==1: 
-            player_pos[0].y+=300*dt
-        elif player_pos[0].y>=screen.get_height()-30 and player_pos[2]==1 :
-            player_pos[2]=-1
-        elif player_pos[0].y>=screen.get_height()-30 and player_pos[2]==-1 :
-            player_pos[0].y-=300*dt
-        elif player_pos[0].y>0+30 and player_pos[2]==-1: 
-            player_pos[0].y-=300*dt
-        elif player_pos[0].y<=0+30 and player_pos[2]==-1: 
-            player_pos[2]=1
+        if abs(ball.get_x()-racket_1.get_coord_x())<=30 and not (ball.get_y()>racket_1.get_coord_top()[1] or ball.get_y()<racket_1.get_coord_bottom()[1]) and ball.get_speed_x()==-1: 
+            ball.set_speed_x(1)
+        if abs(ball.get_x()-racket_1.get_coord_x())<=30 and not (ball.get_y()>racket_1.get_coord_top()[1] or ball.get_y()<racket_1.get_coord_bottom()[1]) and ball.get_speed_x()==1: 
+            ball.set_x(ball.get_x()+300*dt)
+    
+    
+
+
+    
+    #Interaction de la balle avec les surfaces verticales
+        if ball.get_y()<screen.get_height()-30 and ball.get_speed_y()==1: 
+            ball.set_y(ball.get_y()+300*dt)
+        elif ball.get_y()>=screen.get_height()-30 and ball.get_speed_y()==1 :
+            ball.set_speed_y(-1)
+        elif ball.get_y()>=screen.get_height()-30 and ball.get_speed_y()==-1 :
+            ball.set_y(ball.get_y()-300*dt)
+        elif ball.get_y()>0+30 and ball.get_speed_y()==-1: 
+            ball.set_y(ball.get_y()-300*dt)
+        elif ball.get_y()<=0+30 and ball.get_speed_y()==-1: 
+            ball.set_speed_y(1)
     
 
 
